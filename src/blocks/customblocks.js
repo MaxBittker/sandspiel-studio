7c57dd5bb3f7f175b23844928762d48b527dbb65
Luke Wilson <l2wilson94@gmail.com>
2022-03-16 11:10:30 +0000

    plus image data

--- src/blocks/customblocks.js
+++ src/blocks/customblocks.js
@@ -102,7 +102,7 @@ Blockly.Blocks['group'] = {
     this.appendValueInput("ITEM1")
         .setCheck(["Number", "String", "Boolean", "Vector", "Element", "Group"]);
     this.appendDummyInput()
-        .appendField(new Blockly.FieldLabelSerializable("➕"), "NAME");
+        .appendField(new Blockly.FieldImage("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ij48cGF0aCBkPSJNMTggMTBoLTR2LTRjMC0xLjEwNC0uODk2LTItMi0ycy0yIC44OTYtMiAybC4wNzEgNGgtNC4wNzFjLTEuMTA0IDAtMiAuODk2LTIgMnMuODk2IDIgMiAybDQuMDcxLS4wNzEtLjA3MSA0LjA3MWMwIDEuMTA0Ljg5NiAyIDIgMnMyLS44OTYgMi0ydi00LjA3MWw0IC4wNzFjMS4xMDQgMCAyLS44OTYgMi0ycy0uODk2LTItMi0yeiIgZmlsbD0id2hpdGUiIC8+PC9zdmc+Cg==", 15, 15, { alt: "*", flipRtl: "FALSE" }));
     this.setInputsInline(true);
     this.setOutput(true, "Group");
     this.setColour(160);
@@ -302,7 +302,7 @@ Blockly.Blocks['set_data_cell'] = {
           y.render();
 
           shadow.render();
-        }
+        }        
         
       } else if (newValue !== "WIND" && block.getColour() !== "#5b80a5") {
         block.setColour(210);
