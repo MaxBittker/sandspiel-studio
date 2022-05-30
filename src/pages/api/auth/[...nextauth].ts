9c6435513ec736761dd3530c4f2191c7af852f93
max bittker <maxbittker@gmail.com>
2022-05-30 17:24:57 -0400

    not smtp

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,12 +3,17 @@ import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 
-// import nodemailer from "nodemailer";
-// import sgTransport from "nodemailer-sendgrid-transport";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
-const emailHost = "smtp.sendgrid.net";
-const emailUsername = "apikey"; // <- don't replace "apikey" it's the actual username
-const emailPassword = process.env.SENDGRID_API_KEY;
+// api key https://sendgrid.com/docs/Classroom/Send/api_keys.html
+var options = {
+  auth: {
+    api_key: process.env.SENDGRID_API_KEY,
+  },
+};
+
+var transport = nodemailer.createTransport(sgTransport(options));
 
 const prisma = new PrismaClient();
 // For more information on each option (and a full list of options) go to
@@ -18,8 +23,8 @@ export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
-      server: `smtp://${emailUsername}:${emailPassword}@${emailHost}:587`,
       from: "noreply@sandspiel.club",
+      sendVerificationRequest,
     }),
   ],
   theme: {
@@ -35,3 +40,76 @@ export default NextAuth({
   },
   debug: true,
 });
+
+async function sendVerificationRequest({
+  identifier: email,
+  url,
+  expires,
+  provider,
+  token,
+}) {
+  const { host } = new URL(url);
+  await transport.sendMail({
+    to: email,
+    from: "noreply@sandspiel.club",
+    subject: `Sign in to ${host}`,
+    text: text({ url, host }),
+    html: html({ url, host, email }),
+  });
+}
+
+// Email HTML body
+function html({ url, host, email }: Record<"url" | "host" | "email", string>) {
+  // Insert invisible space into domains and email address to prevent both the
+  // email address and the domain from being turned into a hyperlink by email
+  // clients like Outlook and Apple mail, as this is confusing because it seems
+  // like they are supposed to click on their email address to sign in.
+  const escapedEmail = `${email.replace(/\./g, "&#8203;.")}`;
+  const escapedHost = `${host.replace(/\./g, "&#8203;.")}`;
+
+  // Some simple styling options
+  const backgroundColor = "#f9f9f9";
+  const textColor = "#444444";
+  const mainBackgroundColor = "#ffffff";
+  const buttonBackgroundColor = "#346df1";
+  const buttonBorderColor = "#346df1";
+  const buttonTextColor = "#ffffff";
+
+  return `
+<body style="background: ${backgroundColor};">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        <strong>${escapedHost}</strong>
+      </td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        Sign in to sandspiel.club as <strong>${escapedEmail}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>
+`;
+}
+
+// Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
+function text({ url, host }: Record<"url" | "host", string>) {
+  return `Sign in to ${host}\n${url}\n\n`;
+}
