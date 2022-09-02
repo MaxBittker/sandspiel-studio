bb12646b0eea03765f29bd28169a13737a917360
Luke Wilson <l2wilson94@gmail.com>
2022-09-02 14:38:24 +0100

    route old post urls to the new id query url

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -1,3 +1,14 @@
+import { useRouter } from "next/router.js";
 import Browse from "../browse.js";
 
-export default Browse;
+function Post() {
+  const router = useRouter();
+  console.log(router.query);
+  let href = `${window.location.protocol}//${window.location.host}/?id=${router.query.id}`;
+  if (router.query.edit !== undefined) {
+    href += `&edit=${router.query.edit}`;
+  }
+  router.push(href);
+  return <Browse />;
+}
+export default Post;
