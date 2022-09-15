cab07727b15a8a2fa263cc742b34ac7586b471d0
Luke Wilson <l2wilson94@gmail.com>
2022-09-15 11:08:23 +0100

    change world size buttons to dropdown

--- src/simulation-controls/WorldSizeButtons.js
+++ src/simulation-controls/WorldSizeButtons.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Dropdown from "react-dropdown";
 import {
   seedNewWorldSize,
   width,
@@ -6,54 +7,34 @@ import {
   seedWithBorder,
 } from "../simulation/SandApi.js";
 let worldScaleMap = [1 / 4, 1 / 2, 1];
+let worldScaleNameMap = ["small", "big", "MEGA"];
 import { globalState, useStore } from "../store";
 
 const WorldSizeButtons = ({}) => {
   const worldWidth = useStore((state) => state.worldWidth);
   const setWorldSize = useStore((state) => state.setWorldSize);
   const setWorldScale = useStore((state) => state.setWorldScale);
+  const worldScale = useStore((state) => state.worldScale);
+  const choiceId = worldScaleMap.indexOf(worldScale);
+  const choiceName = worldScaleNameMap[choiceId];
 
   return (
     <span className="worldSizes">
-      {worldScaleMap.map((v, i) => (
-        <button
-          key={i}
-          className={v * width == worldWidth ? "selected" : ""}
-          onClick={(e) => {
-            const oldSize = worldWidth;
-            const newSize = Math.round(v * width);
-            if (newSize === oldSize) return;
+      <Dropdown
+        options={worldScaleNameMap.map((label, value) => ({ value, label }))}
+        onChange={(e) => {
+          const oldSize = worldWidth;
+          const newScale = worldScaleMap[e.value];
+          const newSize = Math.round(newScale * width);
+          setWorldSize([newSize, newSize]);
+          globalState.worldWidth = newSize;
+          globalState.worldHeight = newSize;
 
-            // Store world size in two places for performance reasons
-            setWorldSize([newSize, newSize]);
-            globalState.worldWidth = newSize;
-            globalState.worldHeight = newSize;
-
-            seedNewWorldSize(oldSize, newSize);
-            setWorldScale(v);
-            //seedWithBorder();
-          }}
-          style={{
-            padding: "0px",
-            marginRight: i === 0 ? 0 : -1,
-            marginRight: i === worldScaleMap.length - 1 ? 2 : 0,
-            borderTopRightRadius: i < worldScaleMap.length - 1 ? 0 : "",
-            borderBottomRightRadius: i < worldScaleMap.length - 1 ? 0 : "",
-            borderTopLeftRadius: i > 0 ? 0 : "",
-            borderBottomLeftRadius: i > 0 ? 0 : "",
-            marginTop: "2px",
-          }}
-        >
-          <svg height="23" width="23" id="d" viewBox="0 0 100 100">
-            <rect
-              x={100 / 2 - (30 * (i + 1)) / 2}
-              y={100 / 2 - (30 * (i + 1)) / 2}
-              width={30 * (i + 1)}
-              height={30 * (i + 1)}
-            />
-          </svg>
-        </button>
-      ))}
+          seedNewWorldSize(oldSize, newSize);
+          setWorldScale(newScale);
+        }}
+        value={choiceName}
+      />
     </span>
   );
 };
